0d5f36ba35d8560fa16b8626ea888f6abfcc1771
ClassWYZ <wenyuzhang@outlook.com>
2015-04-01 02:23:37 -0400

    basic html

---
+++ js/namelist.js
@@ -0,0 +1,20 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "info.txt";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += arr[i].name;
+    }
+    document.getElementById("id01").innerHTML = out;
+}
